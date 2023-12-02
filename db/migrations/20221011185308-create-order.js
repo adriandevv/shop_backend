0b072878559f8828c908a623c17abb3585f655b1
Adrian Valencia Hernandez <adrian15996@gmail.com>
2023-12-02 16:22:04 -0600

    bug error corrected

--- db/migrations/20221011185308-create-order.js
+++ db/migrations/20221011185308-create-order.js
@@ -1,10 +1,36 @@
 'use strict';
 
-const { ORDER_TABLE, OrderSchema } = require('./../models/order.model');
-
+const { ORDER_TABLE } = require('./../models/order.model');
+const { CUSTOMER_TABLE } = require('./../models/customer.model');
 module.exports = {
-  up: async (queryInterface) => {
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+  up: async (queryInterface, sequelize ) => {
+    await queryInterface.createTable(ORDER_TABLE, 
+      
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: sequelize.DataTypes.INTEGER,
+        },
+        customerId: {
+          allowNull: false,
+          field: 'cusotmer_id',
+          type: sequelize.DataTypes.INTEGER,
+          refernces: {
+            model: CUSTOMER_TABLE,
+            key: 'id',
+          },
+        },
+      
+        createdAt: {
+          allowNull: false,
+          type: sequelize.DataTypes.DATE,
+          field: 'created_at',
+          defaultValue: sequelize.NOW,
+        }
+      }
+      );
   },
 
   down: async (queryInterface) => {
